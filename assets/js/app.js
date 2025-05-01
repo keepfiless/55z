f38f23c27661a413e7b61f04afc2b3049b1b7933
FILES <153207189+keepfiless@users.noreply.github.com>
2025-05-01 15:25:22 +0330

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,9 @@
-    function toggleLanguages(id) {
-      const element = document.getElementById(id);
-      element.classList.toggle('active');
-    }
+// for roadmap
+function toggleLanguages(id) {
+  const element = document.getElementById(id);
+  element.classList.toggle('active');
+}
+// Mobile menu toggle
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('.menu').classList.toggle('active');
+});
