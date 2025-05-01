0c5714d41ba12a8b65a5583c577c28fe7d9ff1f2
sam nikzad <abbas.nikzad70@gmail.com>
2025-05-01 17:36:17 +0330

    ssss

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,3 +13,4 @@ function toggleLanguages(id) {
 document.querySelector('.menu-toggle').addEventListener('click', function() {
     document.querySelector('.menu').classList.toggle('active');
 });
+//  this is comment
